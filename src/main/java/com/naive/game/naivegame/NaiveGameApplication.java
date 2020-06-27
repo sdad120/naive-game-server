f08adf2d856bd6a091f985c01eadd242d9447b63
lawrence lin <lawrence.lin@airbnb.com>
2020-06-27 14:37:18 +0800

    init spring boot with gradle

---
+++ src/main/java/com/naive/game/naivegame/NaiveGameApplication.java
@@ -0,0 +1,13 @@
+package com.naive.game.naivegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaiveGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaiveGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/naive/game/naivegame/NaiveGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naive.game.naivegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaiveGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
